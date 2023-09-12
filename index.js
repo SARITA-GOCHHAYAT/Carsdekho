426d1fba300c541c0b663a69200370686c735047
SARITA-GOCHHAYAT <saritagochhayat7377@gmail.com>
2023-09-12 12:08:14 +0530

    check

---
+++ index.js
@@ -0,0 +1,6 @@
+let hamburger = document.getElementById("menuicon");
+        let wrapper = document.getElementById("wraper");
+hamburger.addEventListener("click",function(){
+    hamburger.style.display="none";
+    wrapper.style.display="block";
+})
