322cbb3fdaee15af23757ba79a41f2942af7786a
SARITA-GOCHHAYAT <saritagochhayat7377@gmail.com>
2023-09-12 18:06:26 +0530

    check

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 let hamburger = document.getElementById("menuicon");
-        let wrapper = document.getElementById("wraper");
+let closed = document.getElementById("close");
+        let wrapper = document.getElementById("option");
 hamburger.addEventListener("click",function(){
     hamburger.style.display="none";
-    wrapper.style.display="block";
+    wrapper.style.display="flex";
+    wrapper.style.flexDirection="column";
+    wrapper.style.marginRight="20%";
+    wrapper.style.marginTop="2%";
+    wrapper.style.marginBottom="2%";
+   wrapper.style.display="block";
+});
+
+closed.addEventListener("click",function(){
+    hamburger.style.display="block";
+    wrapper.style.display="none";
 })
+
+
+
+
+
